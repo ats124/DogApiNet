4fde27f60dbc0b64dc9d31b10da2cdda868948cd
Atsushi Tanaka <ats124@section-9.tech>
2018-05-27 17:22:24 +0900

    test code: Monitor.CreateAsync, Monitor.GetAsync, Monitor.EditAsync, Monitor.DeleteAsync, Monitor.GetAllAsync,

--- sandbox/ConsoleCoreApp/Program.cs
+++ sandbox/ConsoleCoreApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Serialization;
 using Utf8Json;
 using DogApiNet;
@@ -8,66 +10,184 @@ using System.Threading.Tasks;
 
 namespace ConsoleCoreApp
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var apiKey = Environment.GetEnvironmentVariable("DATADOG_API_KEY");
             var appKey = Environment.GetEnvironmentVariable("DATADOG_APP_KEY");
 
             using (var client = new DogApiClient(apiKey, appKey))
             {
-                var monitor = client.Monitor.CreateAsync(new DogMonitorCreateParameter(DogMonitorTypes.MericAlert, "max(last_1m):avg:test.random{*} > 90")
-                {
-                    Name = "metric alert testtest",
-                    Message = "test alert message",
-                    Options = new DogMonitorOptions()
+                var tags = new[] { "testtag1", "testtag2" };
+                var silenced = DateTimeOffset.Now.AddHours(5);
+                var monitor = await client.Monitor.CreateAsync(
+                    new DogMonitorCreateParameter(DogMonitorTypes.MericAlert, "max(last_1m):avg:test.random{*} > 90")
                     {
-                        NotifyNoData = true,
-                        Locked = true,
-                        NewHostDelay = 180,
-                        RenotifyInterval = 30,
-                        NoDataTimeframe = 3,
-                        EscalationMessage = "hogehoge",
-                        RequireFullWindow = false,
-                        EvaluationDelay = 5,
-                        Thresholds = new Dictionary<string, double>()
+                        Name = "metric alert testtest",
+                        Message = "test alert message",
+                        Tags = tags,
+                        Options = new DogMonitorOptions()
                         {
-                            { "critical", 90 },
-                            { "warning", 50 },
-                            { "critical_recovery", 70 },
-                            { "warning_recovery", 30 },
-                        },
-                        Silenced = new Dictionary<string, DateTimeOffset?>()
-                        {
-                            { "*", DateTimeOffset.Now.AddHours(5) }
+                            IncludeTags = true,
+                            TimeoutHours = 2,
+                            NotifyAudit = true,
+                            NotifyNoData = true,
+                            Locked = true,
+                            NewHostDelay = 180,
+                            RenotifyInterval = 30,
+                            NoDataTimeframe = 3,
+                            EscalationMessage = "hogehoge",
+                            RequireFullWindow = true,
+                            EvaluationDelay = 5,
+                            Thresholds = new Dictionary<string, double>()
+                            {
+                                {"critical", 90},
+                                {"warning", 50},
+                                {"critical_recovery", 70},
+                                {"warning_recovery", 30},
+                            },
+                            Silenced = new Dictionary<string, DateTimeOffset?>()
+                            {
+                                {"*", silenced}
+                            }
                         }
-                    }
-                }).Result;
+                    });
+                Debug.Assert(monitor.Query == "max(last_1m):avg:test.random{*} > 90");
+                Debug.Assert(monitor.Name == "metric alert testtest");
+                Debug.Assert(monitor.Message == "test alert message");
+                Debug.Assert(monitor.Tags[0] == tags[0]);
+                Debug.Assert(monitor.Tags[1] == tags[1]);
+                Debug.Assert(monitor.Options.IncludeTags);
+                Debug.Assert(monitor.Options.TimeoutHours == 2);
+                Debug.Assert(monitor.Options.NotifyAudit);
+                Debug.Assert(monitor.Options.NotifyNoData);
+                Debug.Assert(monitor.Options.Locked);
+                Debug.Assert(monitor.Options.NewHostDelay == 180);
+                Debug.Assert(monitor.Options.RenotifyInterval == 30);
+                Debug.Assert(monitor.Options.NoDataTimeframe == 3);
+                Debug.Assert(monitor.Options.EscalationMessage == "hogehoge");
+                Debug.Assert(monitor.Options.RequireFullWindow);
+                Debug.Assert(monitor.Options.Thresholds["critical"] == 90.0);
+                Debug.Assert(monitor.Options.Thresholds["warning"] == 50);
+                Debug.Assert(monitor.Options.Thresholds["critical_recovery"] == 70);
+                Debug.Assert(monitor.Options.Thresholds["warning_recovery"] == 30);
+                Debug.Assert(monitor.Options.Silenced["*"].Value.Normalize() ==  silenced.Normalize());
 
-                var editMonitor = client.Monitor.EditAsync(monitor.Id, new DogMonitorEditParameter("max(last_1m):avg:test.random{*} > 80")
-                {
-                    Options = new DogMonitorOptions()
+                var getMonitor = await client.Monitor.GetAsync(monitor.Id, groupStates: "all");
+                Debug.Assert(getMonitor.Query == "max(last_1m):avg:test.random{*} > 90");
+                Debug.Assert(getMonitor.Name == "metric alert testtest");
+                Debug.Assert(getMonitor.Message == "test alert message");
+                Debug.Assert(getMonitor.Tags[0] == tags[0]);
+                Debug.Assert(getMonitor.Tags[1] == tags[1]);
+                Debug.Assert(getMonitor.Options.IncludeTags);
+                Debug.Assert(getMonitor.Options.TimeoutHours == 2);
+                Debug.Assert(getMonitor.Options.NotifyAudit);
+                Debug.Assert(getMonitor.Options.NotifyNoData);
+                Debug.Assert(getMonitor.Options.Locked);
+                Debug.Assert(getMonitor.Options.NewHostDelay == 180);
+                Debug.Assert(getMonitor.Options.RenotifyInterval == 30);
+                Debug.Assert(getMonitor.Options.NoDataTimeframe == 3);
+                Debug.Assert(getMonitor.Options.EscalationMessage == "hogehoge");
+                Debug.Assert(getMonitor.Options.RequireFullWindow);
+                Debug.Assert(getMonitor.Options.Thresholds["critical"] == 90);
+                Debug.Assert(getMonitor.Options.Thresholds["warning"] == 50);
+                Debug.Assert(getMonitor.Options.Thresholds["critical_recovery"] == 70);
+                Debug.Assert(getMonitor.Options.Thresholds["warning_recovery"] == 30);
+                Debug.Assert(getMonitor.Options.Silenced["*"].Value.Normalize() == silenced.Normalize());
+
+                var tags2 = new[] { "testtag3" };
+                var silenced2 = DateTimeOffset.Now.AddHours(6);
+                var editMonitor = await client.Monitor.EditAsync(monitor.Id,
+                    new DogMonitorEditParameter("max(last_1m):avg:test.random{*} > 80")
                     {
-                        Thresholds = new Dictionary<string, double>()
+                        Name = "metric alert edited",
+                        Message = "test alert message edited",
+                        Tags = tags2,
+                        Options = new DogMonitorOptions()
                         {
-                            { "critical", 80 },
-                            { "warning", 40 },
-                            { "critical_recovery", 60 },
-                            { "warning_recovery", 20 },
-                        },
-                        Silenced = null,
-                    }
-                }).Result;
+                            IncludeTags = false,
+                            TimeoutHours = 3,
+                            NotifyAudit = false,
+                            NotifyNoData = false,
+                            Locked = false,
+                            NewHostDelay = 150,
+                            RenotifyInterval = 10,
+                            NoDataTimeframe = 2,
+                            EscalationMessage = "hogehoge edited",
+                            RequireFullWindow = false,
+                            EvaluationDelay = 3,
+                            Thresholds = new Dictionary<string, double>()
+                            {
+                                {"critical", 80},
+                                {"warning", 40},
+                                {"critical_recovery", 60},
+                                {"warning_recovery", 20},
+                            },
+                            Silenced = new Dictionary<string, DateTimeOffset?>()
+                            {
+                                {"*", silenced2}
+                            }
+                        }
+                    });
+                Debug.Assert(editMonitor.Query == "max(last_1m):avg:test.random{*} > 80");
+                Debug.Assert(editMonitor.Name == "metric alert edited");
+                Debug.Assert(editMonitor.Message == "test alert message edited");
+                Debug.Assert(editMonitor.Tags.Length == tags2.Length);
+                Debug.Assert(editMonitor.Tags[0] == tags2[0]);
+                Debug.Assert(!editMonitor.Options.IncludeTags);
+                Debug.Assert(editMonitor.Options.TimeoutHours == 3);
+                Debug.Assert(!editMonitor.Options.NotifyAudit);
+                Debug.Assert(editMonitor.Options.NotifyNoData == false);
+                Debug.Assert(editMonitor.Options.Locked == false);
+                Debug.Assert(editMonitor.Options.NewHostDelay == 150);
+                Debug.Assert(editMonitor.Options.RenotifyInterval == 10);
+                Debug.Assert(editMonitor.Options.NoDataTimeframe == 2);
+                Debug.Assert(editMonitor.Options.EscalationMessage == "hogehoge edited");
+                Debug.Assert(editMonitor.Options.RequireFullWindow == false);
+                Debug.Assert(editMonitor.Options.Thresholds["critical"] == 80);
+                Debug.Assert(editMonitor.Options.Thresholds["warning"] == 40);
+                Debug.Assert(editMonitor.Options.Thresholds["critical_recovery"] == 60);
+                Debug.Assert(editMonitor.Options.Thresholds["warning_recovery"] == 20);
+                Debug.Assert(editMonitor.Options.Silenced["*"].Value.Normalize() ==  silenced2.Normalize());
 
-                var getMonitor = client.Monitor.GetAsync(monitor.Id, groupStates: "all").Result;
+                var getAllMonitors = await client.Monitor.GetAllAsync(groupStates: "all");
+                getMonitor = getAllMonitors.FirstOrDefault(x => x.Id == editMonitor.Id);
+                Debug.Assert(getMonitor.Query == "max(last_1m):avg:test.random{*} > 80");
+                Debug.Assert(getMonitor.Name == "metric alert edited");
+                Debug.Assert(getMonitor.Message == "test alert message edited");
+                Debug.Assert(getMonitor.Tags.Length == tags2.Length);
+                Debug.Assert(getMonitor.Tags[0] == tags2[0]);
+                Debug.Assert(!editMonitor.Options.IncludeTags);
+                Debug.Assert(editMonitor.Options.TimeoutHours == 3);
+                Debug.Assert(!editMonitor.Options.NotifyAudit);
+                Debug.Assert(getMonitor.Options.NotifyNoData == false);
+                Debug.Assert(getMonitor.Options.Locked == false);
+                Debug.Assert(getMonitor.Options.NewHostDelay == 150);
+                Debug.Assert(getMonitor.Options.RenotifyInterval == 10);
+                Debug.Assert(getMonitor.Options.NoDataTimeframe == 2);
+                Debug.Assert(getMonitor.Options.EscalationMessage == "hogehoge edited");
+                Debug.Assert(getMonitor.Options.RequireFullWindow == false);
+                Debug.Assert(getMonitor.Options.Thresholds["critical"] == 80);
+                Debug.Assert(getMonitor.Options.Thresholds["warning"] == 40);
+                Debug.Assert(getMonitor.Options.Thresholds["critical_recovery"] == 60);
+                Debug.Assert(getMonitor.Options.Thresholds["warning_recovery"] == 20);
+                Debug.Assert(getMonitor.Options.Silenced["*"].Value.Normalize() == silenced2.Normalize());
 
-                var deleteMonitor = client.Monitor.DeleteAsync(monitor.Id).Result;
+                foreach (var m in getAllMonitors)
+                {
+                    var deleteMonitor = await client.Monitor.DeleteAsync(m.Id);
+                }
 
-                var getAllMonitors = client.Monitor.GetAllAsync(groupStates: "all").Result;
-
-                //var result = client.Monitor.ResolveAsync(new[] { new DogMonitorResolve(3505039, "*") }).Result;
+                getAllMonitors = await client.Monitor.GetAllAsync(groupStates: "all");
+                Debug.Assert(getAllMonitors.Length == 0);
             }
+        }
+
+        private static DateTimeOffset Normalize(this DateTimeOffset @this)
+        {
+            @this = @this.UtcDateTime;
+            return new DateTimeOffset(@this.Year, @this.Month, @this.Day, @this.Hour, @this.Minute, @this.Second, TimeSpan.Zero);
         }
     }
 }
