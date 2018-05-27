ab3e267c9de29f192703680501cdbd1b03a237c6
Unknown <ats124@section-9.tech>
2018-05-27 22:45:35 +0900

    add: Downtime Api file

---
+++ src/DogApiNet/DogApiClient_Downtime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Utf8Json;
+using Utf8Json.Formatters;
+
+namespace DogApiNet
+{
+    using JsonFormatters;
+
+    public interface IDowntimeApi
+    {
+    }
+
+    partial class DogApiClient : IDowntimeApi
+    {
+        public IServiceCheckApi IDowntime => this;
+
+    }
+}
