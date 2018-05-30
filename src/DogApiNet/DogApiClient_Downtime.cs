03088a2e72f28c9397c2019ceba9677a57e884fc
Unknown <ats124@section-9.tech>
2018-05-30 23:04:55 +0900

    add: Downtime.GetAllAsync

--- src/DogApiNet/DogApiClient_Downtime.cs
+++ src/DogApiNet/DogApiClient_Downtime.cs
@@ -16,11 +16,95 @@ namespace DogApiNet
 
     public interface IDowntimeApi
     {
+        Task<DogDowntimeGetResult[]> GetAllAsync(bool? currentOnly = null, CancellationToken? cancelToken = null);
+    }
+
+    public static class DogDowntimeRecurrenceTypes
+    {
+        public static readonly string Days = "days";
+        public static readonly string Weeks = "weeks";
+        public static readonly string Months = "months";
+        public static readonly string Years = "years";
+    }
+
+    [JsonFormatter(typeof(OptionalPropertySupportFormatter<DogDowntimeRecurrence>))]
+    public class DogDowntimeRecurrence : OptionalPropertySupport<DogDowntimeRecurrence>
+    {
+        [DataMember(Name = "type")]
+        public string Type { get => GetValue<string>(); set => SetValue(value); }
+
+        [DataMember(Name = "period")]
+        public int Period { get => GetValue<int>(); set => SetValue(value); }
+
+        [DataMember(Name = "week_days")]
+        [JsonFormatter(typeof(WeekDaysFormatter))]
+        public DayOfWeek[] WeekDays { get => GetValue<DayOfWeek[]>(); set => SetValue(value); }
+
+        [DataMember(Name = "until_date")]
+        [JsonFormatter(typeof(NullableUnixTimeSecondsDateTimeOffsetFormatter))]
+        public DateTimeOffset? UntilDate { get => GetValue<DateTimeOffset?>(); set => SetValue(value); }
+
+        [DataMember(Name = "until_occurrences")]
+        public int? UntilOccurrences { get => GetValue<int?>(); set => SetValue(value); }
+    }
+
+    public class DogDowntimeGetResult
+    {
+        [DataMember(Name = "active")]
+        public bool Active { get; set; }
+
+        [DataMember(Name = "canceled")]
+        [JsonFormatter(typeof(NullableUnixTimeSecondsDateTimeOffsetFormatter))]
+        public DateTimeOffset? Canceled { get; set; }
+
+        [DataMember(Name = "creator_id")]
+        public long? CreatorId { get; set; }
+
+        [DataMember(Name = "updater_id")]
+        public long? UpdaterId { get; set; }
+
+        [DataMember(Name = "parent_id")]
+        public long? ParentId { get; set; }
+
+        [DataMember(Name = "disabled")]
+        public bool Disabled { get; set; }
+
+        [DataMember(Name = "scope")]
+        public string[] Scope { get; set; }
+
+        [DataMember(Name = "monitor_id")]
+        public long? MonitorId { get; set; }
+
+        [DataMember(Name = "message")]
+        public string Message { get; set; }
+
+        [DataMember(Name = "start")]
+        [JsonFormatter(typeof(NullableUnixTimeSecondsDateTimeOffsetFormatter))]
+        public DateTimeOffset? Start { get; set; }
+
+        [DataMember(Name = "end")]
+        [JsonFormatter(typeof(NullableUnixTimeSecondsDateTimeOffsetFormatter))]
+        public DateTimeOffset? End { get; set; }
+
+        [DataMember(Name = "recurrence")]
+        public DogDowntimeRecurrence Recurrence { get; set; }
+
+        [DataMember(Name = "timezone")]
+        public string TimeZone { get; set; }
     }
 
     partial class DogApiClient : IDowntimeApi
     {
-        public IServiceCheckApi IDowntime => this;
+        public IDowntimeApi Downtime => this;
 
+        async Task<DogDowntimeGetResult[]> IDowntimeApi.GetAllAsync(bool? currentOnly, CancellationToken? cancelToken)
+        {
+            var @params = new NameValueCollection();
+            if (currentOnly != null)
+            {
+                @params.Add("current_only", currentOnly.Value.ToString().ToLowerInvariant());
+            }
+            return await RequestAsync<DogDowntimeGetResult[]>(HttpMethod.Get, "/api/v1/downtime", @params, null, cancelToken).ConfigureAwait(false);
+        }
     }
 }
---
+++ src/DogApiNet/JsonFormatters/WeekDaysFormatter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Utf8Json;
+using Utf8Json.Formatters;
+
+namespace DogApiNet.JsonFormatters
+{
+    public class WeekDaysFormatter : IJsonFormatter<DayOfWeek[]>
+    {
+        private static readonly IDictionary<string, DayOfWeek> NameValueMappings;
+        private static readonly IDictionary<DayOfWeek, string> ValueNameMappings;
+        private readonly ArrayFormatter<string> _internalFormatter = new ArrayFormatter<string>();
+
+        static WeekDaysFormatter()
+        {
+            ValueNameMappings = new Dictionary<DayOfWeek, string>
+            {
+                {DayOfWeek.Sunday, "Sun"},
+                {DayOfWeek.Monday, "Mon"},
+                {DayOfWeek.Tuesday, "Tue"},
+                {DayOfWeek.Wednesday, "Wed"},
+                {DayOfWeek.Thursday, "Thu"},
+                {DayOfWeek.Friday, "Fri"},
+                {DayOfWeek.Saturday, "Sat"},
+            };
+            NameValueMappings = ValueNameMappings.ToDictionary(x => x.Value, x => x.Key);
+        }
+
+        public DayOfWeek[] Deserialize(ref JsonReader reader, IJsonFormatterResolver formatterResolver) =>
+            _internalFormatter.Deserialize(ref reader, formatterResolver)?.Select(x => NameValueMappings[x]).ToArray();
+
+        public void Serialize(ref JsonWriter writer, DayOfWeek[] value, IJsonFormatterResolver formatterResolver) =>
+            _internalFormatter.Serialize(ref writer, value?.Select(x => ValueNameMappings[x]).ToArray(),
+                formatterResolver);
+    }
+}
