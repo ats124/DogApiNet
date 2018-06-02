3e07c608a3c080cd0f850fabd4f026f19f516118
Atsushi Tanaka <ats124@section-9.tech>
2018-06-02 13:52:46 +0900

    add: DogApiClient_Timeboard.cs file

---
+++ src/DogApiNet/DogApiClient_Timeboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Utf8Json;
+using Utf8Json.Formatters;
+
+namespace DogApiNet
+{
+    using JsonFormatters;
+
+    public interface ITimeboardApi
+    {
+    }
+
+    partial class DogApiClient : ITimeboardApi
+    {
+        public ITimeboardApi Timeboard => this;
+    }
+}
