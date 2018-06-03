2147a8df6881666bbe234f4070996c7a31197924
Unknown <ats124@section-9.tech>
2018-06-03 15:27:37 +0900

    refactored: Metadata API

--- src/DogApiNet/DogApiClient_Metadata.cs
+++ src/DogApiNet/DogApiClient_Metadata.cs
@@ -1,78 +1,32 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Runtime.Serialization;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Utf8Json;
-using Utf8Json.Formatters;
 
 namespace DogApiNet
 {
-    using JsonFormatters;
-
     public interface IMetadataApi
     {
-        Task<DogMetadataGetResult> GetAsync(string metric, CancellationToken? cancelToken = null);
+        Task<DogMetadata> GetAsync(string metric, CancellationToken? cancelToken = null);
 
-        Task<DogMetadataUpdateResult> UpdateAsync(DogMetadataUpdateParameter param, CancellationToken? cancelToken = null);
+        Task<DogMetadata> UpdateAsync(DogMetadata metadata, CancellationToken? cancelToken = null);
     }
 
-    public class DogMetadataGetResult
+    public class DogMetadata
     {
-        [DataMember(Name = "description")]
-        public string Description { get; set; }
+        public DogMetadata()
+        {
+        }
 
-        [DataMember(Name = "short_name")]
-        public string ShortName { get; set; }
-
-        [DataMember(Name = "statsd_interval")]
-        public int? StatsDInterval { get; set; }
-
-        [DataMember(Name = "per_unit")]
-        public string PerUnit { get; set; }
-
-        [DataMember(Name = "type")]
-        public string Type { get; set; }
-
-        [DataMember(Name = "unit")]
-        public string Unit { get; set; }
-    }
-
-    public class DogMetadataUpdateParameter
-    {
-        [IgnoreDataMember]
-        public string Metric { get; set; }
-
-        [DataMember(Name = "description")]
-        public string Description { get; set; }
-
-        [DataMember(Name = "short_name")]
-        public string ShortName { get; set; }
-
-        [DataMember(Name = "statsd_interval")]
-        public int? StatsDInterval { get; set; }
-
-        [DataMember(Name = "per_unit")]
-        public string PerUnit { get; set; }
-
-        [DataMember(Name = "type")]
-        public string Type { get; set; }
-
-        [DataMember(Name = "unit")]
-        public string Unit { get; set; }
-
-        public DogMetadataUpdateParameter(string metric)
+        public DogMetadata(string metric)
         {
             Metric = metric;
         }
-    }
 
-    public class DogMetadataUpdateResult
-    {
+        [IgnoreDataMember]
+        public string Metric { get; set; }
+
         [DataMember(Name = "description")]
         public string Description { get; set; }
 
@@ -96,15 +50,23 @@ namespace DogApiNet
     {
         public IMetadataApi Metadata => this;
 
-        async Task<DogMetadataGetResult> IMetadataApi.GetAsync(string metric, CancellationToken? cancelToken)
+        async Task<DogMetadata> IMetadataApi.GetAsync(string metric, CancellationToken? cancelToken)
         {
-            return await RequestAsync<DogMetadataGetResult>(HttpMethod.Get, $"/api/v1/metrics/{metric}", null, null, cancelToken).ConfigureAwait(false);
+            var metadata =
+                await RequestAsync<DogMetadata>(HttpMethod.Get, $"/api/v1/metrics/{metric}", null, null, cancelToken)
+                    .ConfigureAwait(false);
+            metadata.Metric = metric;
+            return metadata;
         }
 
-        async Task<DogMetadataUpdateResult> IMetadataApi.UpdateAsync(DogMetadataUpdateParameter param, CancellationToken? cancelToken)
+        async Task<DogMetadata> IMetadataApi.UpdateAsync(DogMetadata metadata, CancellationToken? cancelToken)
         {
-            var data = new DogApiHttpRequestContent("application/json", JsonSerializer.Serialize(param));
-            return await RequestAsync<DogMetadataUpdateResult>(HttpMethod.Put, $"/api/v1/metrics/{param.Metric}", null, data, cancelToken).ConfigureAwait(false);
+            var data = new DogApiHttpRequestContent("application/json", JsonSerializer.Serialize(metadata));
+            var updatedMetadata =
+                await RequestAsync<DogMetadata>(HttpMethod.Put, $"/api/v1/metrics/{metadata.Metric}", null, data,
+                    cancelToken).ConfigureAwait(false);
+            updatedMetadata.Metric = metadata.Metric;
+            return updatedMetadata;
         }
     }
 }
