8d0aad470623f58161a9715f94ff78c756bc0410
Atsushi Tanaka <ats124@section-9.tech>
2018-06-06 21:11:18 +0900

    add: Tag.GetAllAsync

--- sandbox/ConsoleCoreApp/Program.cs
+++ sandbox/ConsoleCoreApp/Program.cs
@@ -19,53 +19,12 @@ namespace ConsoleCoreApp
 
             using (var client = new DogApiClient(apiKey, appKey))
             {
-                var monitor = await client.Monitor.CreateAsync(new DogMonitor(DogMonitorTypes.MericAlert, "max(last_1m):avg:test.random{*} > 80"));
-
-                var start = DateTimeOffset.UtcNow.AddDays(1);
-                var downtime = await client.Downtime.CreateAsync(new DogDowntime("*")
-                {
-                    Start = start,
-                    End = start.AddDays(1),
-                    Message = "test message",
-                    MonitorId = monitor.Id,
-                    Recurrence = new DogDowntimeRecurrence(DogDowntimeRecurrenceTypes.Days, 1)
-                    {
-                        WeekDays = new[] {DayOfWeek.Monday, DayOfWeek.Saturday},
-                        //UntilDate = start.AddDays(10),
-                        UntilOccurrences = 5,
-                    },
-                    TimeZone = "UTC"
-                });
-
-                downtime.Start = downtime.Start.Value.AddDays(1);
-                downtime.End = downtime.Start.Value.AddDays(1);
-                downtime.Message = "test message edit";
-                downtime.MonitorId = null;
-                downtime.Recurrence.WeekDays = new[] {DayOfWeek.Wednesday};
-                downtime.Recurrence.UntilDate = start.AddDays(5);
-                downtime.TimeZone = "Asia/Japan";
-
-                var getDowntime = await client.Downtime.GetAsync(downtime.Id);
-
-                var updateDowntime = await client.Downtime.UpdateAsync(downtime);
-                Debug.Assert(updateDowntime.Scope[0] == downtime.Scope[0]);
-                Debug.Assert(updateDowntime.Start.Value.Normalize() == downtime.Start.Value.Normalize());
-                Debug.Assert(updateDowntime.End.Value.Normalize() == downtime.End.Value.Normalize());
-                Debug.Assert(updateDowntime.Message == downtime.Message);
-                Debug.Assert(updateDowntime.MonitorId == downtime.MonitorId);
-                Debug.Assert(updateDowntime.Recurrence.Type == downtime.Recurrence.Type);
-                Debug.Assert(updateDowntime.Recurrence.Period == downtime.Recurrence.Period);
-                Debug.Assert(updateDowntime.Recurrence.WeekDays[0] == downtime.Recurrence.WeekDays[0]);
-                Debug.Assert(updateDowntime.Recurrence.UntilOccurrences == downtime.Recurrence.UntilOccurrences);
-                Debug.Assert(updateDowntime.TimeZone == downtime.TimeZone);
-
-                var getAllDowntime = await client.Downtime.GetAllAsync();
-                foreach (var d in getAllDowntime)
-                {
-                    await client.Downtime.DeleteAsync(d.Id);
-                }
-
-                await client.Monitor.DeleteAsync(monitor.Id);
+                //var obj = new DogTagGetAllResult()
+                //{
+                //    Tags = new[] {"A", "B"}.ToLookup(x => x)
+                //};
+                //Console.WriteLine(JsonSerializer.ToJsonString(obj));
+                var tags = await client.Tag.GetAll();
             }
         }
 
---
+++ src/DogApiNet/DogApiClient_Tag.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DogApiNet
+{
+    public interface ITagApi
+    {
+        Task<ILookup<string, string>> GetAllAsync(string source = null, CancellationToken? cancelToken = null);
+    }
+
+    public class DogTagGetAllResult
+    {
+        [DataMember(Name = "tags")]
+        public IDictionary<string, string[]> Tags { get; set; }
+    }
+
+    partial class DogApiClient : ITagApi
+    {
+        public ITagApi Tag => this;
+
+        async Task<ILookup<string, string>> ITagApi.GetAllAsync(string source, CancellationToken? cancelToken)
+        {
+            var @params = new NameValueCollection();
+            if (source != null) @params["source"] = source;
+            var result =
+                await RequestAsync<DogTagGetAllResult>(HttpMethod.Get, $"/api/v1/tags/hosts", @params, null,
+                        cancelToken)
+                    .ConfigureAwait(false);
+            if (result?.Tags == null)
+                return new string[0].ToLookup(x => x);
+            return result.Tags
+                .SelectMany(x => x.Value.Select(y => (key: x.Key, value: y)))
+                .ToLookup(x => x.key, x => x.value);
+        }
+    }
+}
