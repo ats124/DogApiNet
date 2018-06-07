5742d763a00ad33311a5f091705fa96a571ba36a
Atsushi Tanaka <ats124@section-9.tech>
2018-06-07 20:18:42 +0900

    add: Tag.CreateAsync, Tag.UpdateAsync

--- src/DogApiNet/DogApiClient_Tag.cs
+++ src/DogApiNet/DogApiClient_Tag.cs
@@ -6,14 +6,22 @@ using System.Net.Http;
 using System.Runtime.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
+using Utf8Json;
 
 namespace DogApiNet
 {
     public interface ITagApi
     {
         Task<ILookup<string, string>> GetAllAsync(string source = null, CancellationToken? cancelToken = null);
+
         Task<string[]> GetAsync(string hostName, string source = null, CancellationToken? cancelToken = null);
-        Task<ILookup<string, string>> GetBySourceAsync(string hostName, string source = null, CancellationToken? cancelToken = null);
+
+        Task<ILookup<string, string>> GetBySourceAsync(string hostName, string source = null,
+            CancellationToken? cancelToken = null);
+
+        Task<string[]> CreateAsync(string hostName, string[] tags, string source = null, CancellationToken? cancelToken = null);
+
+        Task<string[]> UpdateAsync(string hostName, string[] tags, string source = null, CancellationToken? cancelToken = null);
     }
 
     public class DogTagGetAllResult
@@ -32,6 +40,21 @@ namespace DogApiNet
     {
         [DataMember(Name = "tags")]
         public IDictionary<string, string[]> Tags { get; set; }
+    }
+
+    public class DogTagCreateUpdateParameter
+    {
+        [DataMember(Name = "tags")]
+        public string[] Tags { get; set; }
+    }
+
+    public class DogTagCreateUpdateResult
+    {
+        [DataMember(Name = "hostname")]
+        public string[] HostName { get; set; }
+
+        [DataMember(Name = "tags")]
+        public string[] Tags { get; set; }
     }
 
     partial class DogApiClient : ITagApi
@@ -58,19 +81,22 @@ namespace DogApiNet
             var @params = new NameValueCollection();
             if (source != null) @params["source"] = source;
             var result =
-                await RequestAsync<DogTagGetResult>(HttpMethod.Get, $"/api/v1/tags/hosts/{Uri.EscapeDataString(hostName)}", @params, null,
+                await RequestAsync<DogTagGetResult>(HttpMethod.Get,
+                        $"/api/v1/tags/hosts/{Uri.EscapeDataString(hostName)}", @params, null,
                         cancelToken)
                     .ConfigureAwait(false);
             return result?.Tags ?? new string[0];
         }
 
-        async Task<ILookup<string, string>> ITagApi.GetBySourceAsync(string hostName, string source, CancellationToken? cancelToken)
+        async Task<ILookup<string, string>> ITagApi.GetBySourceAsync(string hostName, string source,
+            CancellationToken? cancelToken)
         {
             var @params = new NameValueCollection();
             if (source != null) @params["source"] = source;
             @params["by_source"] = "true";
             var result =
-                await RequestAsync<DogTagGetBySourceResult>(HttpMethod.Get, $"/api/v1/tags/hosts/{Uri.EscapeDataString(hostName)}", @params, null,
+                await RequestAsync<DogTagGetBySourceResult>(HttpMethod.Get,
+                        $"/api/v1/tags/hosts/{Uri.EscapeDataString(hostName)}", @params, null,
                         cancelToken)
                     .ConfigureAwait(false);
             if (result?.Tags == null)
@@ -78,6 +104,38 @@ namespace DogApiNet
             return result.Tags
                 .SelectMany(x => x.Value.Select(y => (key: x.Key, value: y)))
                 .ToLookup(x => x.key, x => x.value);
+        }
+
+        async Task<string[]> ITagApi.CreateAsync(string hostName, string[] tags, string source, CancellationToken? cancelToken)
+        {
+            var @params = new NameValueCollection();
+            if (source != null) @params["source"] = source;
+
+            var data = new DogApiHttpRequestContent("application/json",
+                JsonSerializer.Serialize(new DogTagCreateUpdateParameter {Tags = tags}));
+
+            var result =
+                await RequestAsync<DogTagCreateUpdateResult>(HttpMethod.Post,
+                        $"/api/v1/tags/hosts/{Uri.EscapeDataString(hostName)}", @params, data,
+                        cancelToken)
+                    .ConfigureAwait(false);
+            return result?.Tags ?? new string[0];
+        }
+
+        async Task<string[]> ITagApi.UpdateAsync(string hostName, string[] tags, string source, CancellationToken? cancelToken)
+        {
+            var @params = new NameValueCollection();
+            if (source != null) @params["source"] = source;
+
+            var data = new DogApiHttpRequestContent("application/json",
+                JsonSerializer.Serialize(new DogTagCreateUpdateParameter {Tags = tags}));
+
+            var result =
+                await RequestAsync<DogTagCreateUpdateResult>(HttpMethod.Put,
+                        $"/api/v1/tags/hosts/{Uri.EscapeDataString(hostName)}", @params, data,
+                        cancelToken)
+                    .ConfigureAwait(false);
+            return result?.Tags ?? new string[0];
         }
     }
 }
