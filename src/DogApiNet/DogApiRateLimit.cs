4977d4996f4dae5366c315be9967bf21e139d0b0
Atsushi Tanaka <ats124@section-9.tech>
2018-06-08 21:53:39 +0900

    add: DogApiRateLimit

---
+++ src/DogApiNet/DogApiRateLimit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogApiNet
+{
+    public class DogApiRateLimit
+    {
+        public int Limit { get; }
+        public int Period { get; }
+        public int Remainig { get; }
+        public int Reset { get; }
+
+        public DogApiRateLimit(int limit, int period, int remaining, int reset)
+        {
+            Limit = limit;
+            Period = period;
+            Remainig = remaining;
+            Reset = reset;
+        }
+    }
+}
